230b1e10db11c43779215c54cdbf5cecc64ffcc5
nguyenThiTham98 <nttham98@gmail.com>
2020-08-26 09:37:39 +0700

    Chuong 14

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
 	attr_accessor :remember_token
 	has_many :microposts, dependent: :destroy
+	has_many :active_relationships, class_name: "Relationship",
+									foreign_key: "follower_id",
+									dependent: :destroy
+	has_many :passive_relationships, class_name: "Relationship",
+									foreign_key: "followed_id",
+									dependent: :destroy								
+	has_many :following, through: :active_relationships, source: :followed
+	has_many :followers, through: :passive_relationships, source: :follower
 	before_save :downcase_email
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -43,10 +51,31 @@ class User < ApplicationRecord
 
 	def current_user?(user)
 		user && user == self
-	end		
+	end	
+
+	# Returns true if a password reset has expired.
+	def password_reset_expired?
+		reset_sent_at < 2.hours.ago
+	end	
 
 	def feed
-		self.microposts
+		part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+		Micropost.joins(user: :followers).where(part_of_feed, { id: id })
+	end
+
+	# Follows a user.
+	def follow(other_user)
+		following << other_user
+	end
+
+	# Unfollows a user.
+	def unfollow(other_user)
+		following.delete(other_user)
+	end
+
+	# Returns true if the current user is following the other user.
+	def following?(other_user)
+		following.include?(other_user)
 	end
 
 	private
